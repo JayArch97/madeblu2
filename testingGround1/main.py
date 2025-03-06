ba58babcada1a62b1ba06f8e84faf42318f71a9d
Jayarch97 <jayquintanilla2@gmail.com>
2025-03-06 19:44:19 +0000

    Initial commit

---
+++ testingGround1/main.py
@@ -0,0 +1,12 @@
+import pandas
+import functions_framework
+import datetime
+import sys
+
+
+@functions_framework.http
+def get_time(request):
+  timestamp = datetime.datetime.now().strftime("%H:%M:%S")
+  request_json = request.get_json(silent=True)
+  request_args = request.args
+  return (f"The time is {timestamp}!")
